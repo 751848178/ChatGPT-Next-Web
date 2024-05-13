7ffd2037ebe72eff9186575317599ec66507efaa
494593337@qq.com <494593337@qq.com>
2024-05-13 23:34:22 +0800

    feat: 新增统计

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <script>
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?fdc6e7806ff3a74ef18bbf12baaf4bbf";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+      </script>
+      <script>
+        !(function(c,b,d,a){c[a]||(c[a]={});c[a]={
+          "pid": "fm1uj1xt50@6080a184a9404b6",
+          "endpoint": "https://fm1uj1xt50-default-cn.rum.aliyuncs.com"
+        };
+        with(b)with(body)with(insertBefore(createElement("script"),firstChild))setAttribute("crossorigin","",src=d)
+        })(window, document, "https://sdk.rum.aliyuncs.com/v2/browser-sdk.js", "__rum");
+      </script>
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
